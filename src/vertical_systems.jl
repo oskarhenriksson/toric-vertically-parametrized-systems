ea1fbdc07b362a111791b49e51ab8cdb6d125d7b
Oskar Henriksson <oskar.david.henriksson@gmail.com>
2024-10-23 00:30:06 -0500

    Work over rational function field

--- src/vertical_systems.jl
+++ src/vertical_systems.jl
@@ -2,16 +2,15 @@
 
 # General vertical system functions
 vertical_system = function (C::QQMatrix, M::ZZMatrix)
-    A, a = polynomial_ring(QQ, "a" => 1:ncols(M))
+    A, a = rational_function_field(QQ, "a" => 1:ncols(M))
     R, x = polynomial_ring(A, "x" => 1:nrows(M))
     return C * diagonal_matrix(a) * [prod(x .^ m) for m in eachcol(M)]
 end
 
 # Checks the matrix of monomials for linearity
-function is_linear(B::ZZMatrix)
-    for i in 1:ncols(B)
-        col = B[:,i]
-        if !is_unit_vector(col) && !is_zero(col)
+function is_linear(M::ZZMatrix)
+    for col in eachcol(M)
+        if !is_unit_vector(Vector(col)) && !is_zero(Vector(col))
             return false
         end
     end
